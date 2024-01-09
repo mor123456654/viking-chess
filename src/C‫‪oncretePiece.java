15b7f18b0a80edf96298ecc9c21122623dd40935
noavrd <noavrd@gmail.com>
2024-01-09 20:52:12 +0200

    ConcretePiece

---
+++ src/C‫‪oncretePiece.java
@@ -0,0 +1,11 @@
+public class ConcretePiece implements Piece{
+    @java.lang.Override
+    public Player getOwner() {
+        return null;
+    }
+
+    @java.lang.Override
+    public String getType() {
+        return null;
+    }
+}
