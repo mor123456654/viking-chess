52ff551594ec847047378443eb9591e574bb8049
Noa Vered Shalom <noavrd@gmail.com>
2024-01-31 19:49:41 +0200

    fix

--- src/King.java
+++ src/King.java
@@ -13,15 +13,15 @@ ArrayList <Position> moves=new ArrayList<>();
         return a.isCorner();
 
     }
-public void printMoves(String s){
-        System.out.print("k"+this.playerName+":");
-        System.out.print("(");
-        for(int i=0;i<moves.size();i++){
-            System.out.print(position.get(i).getCol()+","+position.get(i).getRow());
-        }
-    System.out.print(")");
-    System.out.println("\n");
-}
+// public void printMoves(String s){
+//         System.out.print("k"+this.playerName+":");
+//         System.out.print("(");
+//         for(int i=0;i<moves.size();i++){
+//             System.out.print(position.get(i).getCol()+","+position.get(i).getRow());
+//         }
+//     System.out.print(")");
+//     System.out.println("\n");
+// }
     public int getPlayerName(){
         return playerName;
     }
