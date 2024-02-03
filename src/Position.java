08b7a7495317142e94354b7bd28ab9ee597f42b6
Noa Vered Shalom <noavrd@gmail.com>
2024-02-03 23:35:46 +0200

    Update Position.java

--- src/Position.java
+++ src/Position.java
@@ -7,27 +7,34 @@ public class Position {
     private int col;
     Piece piece=null;
     List<ConcretePiece> difPiece= new ArrayList<ConcretePiece>();
+    
     Position(int col,int row){
         this.row= row;
         this.col= col;
     }
-public void addPiece(ConcretePiece p){
+
+    public void addPiece(ConcretePiece p){
         if(!difPiece.contains(p))
             difPiece.add(p);
-}
-public List<ConcretePiece> getDifPiece(){
-        return this.difPiece;
-}
-    public void setPosition(int col, int row){
-            this.row=row;
-            this.col=col;
     }
+
+    public List<ConcretePiece> getDifPiece(){
+        return this.difPiece;
+    }
+
+    public void setPosition(int col, int row){
+        this.row=row;
+        this.col=col;
+    }
+
     public void setPiece(Piece piece,int col, int row){
         this.piece=piece;
         ConcretePiece piece1 = (ConcretePiece) this.piece;
         piece1.addPosition(new Position(col,row));
     }
-    public void setPiece(Piece piece){this.piece=piece;}
+
+    public void setPiece(Piece piece){ this.piece=piece; }
+
     public int getRow() {
         return row;
     }
@@ -37,22 +44,25 @@ public List<ConcretePiece> getDifPiece(){
     }
 
     public boolean isCorner() {
-        if (row == 0 && col == 0 || row == 10 && col == 10 || row == 10 && col == 0||row == 0 && col == 10){
+        if ( row == 0 && col == 0 || row == 10 && col == 10 || row == 10 && col == 0 || row == 0 && col == 10 ){
             return true;
         }
         return false;
     }
+
     public boolean isNearWall() {
         return row - 1 == -1 || col - 1 == -1 || row + 1 == 11 || col + 1 == 11;
     }
-    public boolean isNearCorner() {
-        return (row == 10 && col == 9) ||(row==10&&col==1)||(row==9&&col==10)||(row==0&&col==1)||(row==0&&col==9)||(row==9&&col==0)||(row==1&&col==0)||(row==1&&col==10);
-    }
-    public void isEating(){
 
+    public boolean isNearCorner() {
+        return ( row == 10 && col == 9 ) || ( row == 10 && col == 1 ) || ( row == 9 && col == 10 ) || ( row == 0 && col == 1 ) || ( row == 0 && col == 9 ) || ( row == 9 && col == 0 ) || ( row == 1 && col == 0 ) || ( row == 1 && col == 10 );
     }
+
+    // public void isEating(){
+
+    // }
     public void printMoves(){
-            System.out.println("(" + getCol() + ", " + getRow() + ")"+difPiece.size()+" pieces");
+        System.out.println( "(" + getCol() + ", " + getRow() + ")" + difPiece.size() + " pieces" );
     }
 
 }
